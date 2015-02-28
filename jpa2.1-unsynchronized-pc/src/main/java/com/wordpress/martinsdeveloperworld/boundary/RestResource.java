1d78d321ed1f56430cc0956fde829653b6a02484
Martin Mois <martin.mois@googlemail.com>
2015-02-28 12:35:33 +0100

    added jpa2.1-unsynchronized-pc

---
+++ jpa2.1-unsynchronized-pc/src/main/java/com/wordpress/martinsdeveloperworld/boundary/RestResource.java
@@ -0,0 +1,40 @@
+package com.wordpress.martinsdeveloperworld.boundary;
+
+import com.wordpress.martinsdeveloperworld.control.Controller;
+import com.wordpress.martinsdeveloperworld.entity.Person;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import java.io.Serializable;
+import java.util.List;
+
+@Path("rest")
+@Produces("text/json")
+@SessionScoped
+public class RestResource implements Serializable {
+	@Inject
+	private Controller controller;
+
+	@GET
+	@Path("persist")
+	public Person persist(@Context HttpServletRequest request) {
+		return controller.persist();
+	}
+
+	@GET
+	@Path("list")
+	public List<Person> list() {
+		return controller.list();
+	}
+
+	@GET
+	@Path("commit")
+	public void commit() {
+		controller.commit();
+	}
+}
---
+++ jpa2.1-unsynchronized-pc/src/main/java/com/wordpress/martinsdeveloperworld/control/Controller.java
@@ -0,0 +1,33 @@
+package com.wordpress.martinsdeveloperworld.control;
+
+import com.wordpress.martinsdeveloperworld.entity.Person;
+
+import javax.ejb.Stateful;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+import javax.persistence.SynchronizationType;
+import java.util.List;
+
+@Stateful
+public class Controller {
+	@PersistenceContext(type = PersistenceContextType.EXTENDED, synchronization = SynchronizationType.UNSYNCHRONIZED)
+	EntityManager entityManager;
+
+	public Person persist() {
+		Person p = new Person();
+		p.setFirstName("Martin");
+		p.setLastName("Developer");
+		return entityManager.merge(p);
+	}
+
+	public List<Person> list() {
+		return entityManager.createQuery("from Person", Person.class).getResultList();
+	}
+
+	public void commit() {
+		entityManager.joinTransaction();
+	}
+}
